9d46414448a6572d29a7b5ef373867d45cd422f5
qe13026445 <qe13026445@ldap.com>
2022-01-06 00:54:10 +0530

    asds

--- uschat/settings.py
+++ uschat/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import os
 import django_heroku
 
-django_heroku.setting(locals())
+django_heroku.settings(locals())
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
